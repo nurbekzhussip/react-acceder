2a4b9919318baea5c4086af938c3d61c242c5982
nurbekzhussip <nurbekzhussip@gmail.com>
2022-04-14 16:13:05 +0500

    refactor: update index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { default as ReactAccederProvider } from './lib/components/ReactAcceder';
 import { useAccess } from './lib/components/ReactAcceder.hook';
+import { default as ReactAccederProvider } from './lib/components/ReactAcceder';
 
 export { ReactAccederProvider, useAccess };
